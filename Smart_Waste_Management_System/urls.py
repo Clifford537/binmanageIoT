81d6ecea31015c7069ed85b43da49566f4150b67
Clifford537 <cliffordmukosh@gmail.com>
2025-01-07 00:29:36 -0800

    updates on setttings

--- Smart_Waste_Management_System/urls.py
+++ Smart_Waste_Management_System/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # Add include to include accounts URLs
+from django.urls import path, include
+from django.conf.urls.static import static 
+from django.conf import settings
+ # Add include to include accounts URLs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,4 @@ urlpatterns = [
     path('', include('accounts.urls')),  # Use '' for the home page
 
     # If you want to define other app URLs later, you can add them here
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
