5307e78819ee683aedc9f49cd0bd4a11767de59b
Clifford537 <cliffordmukosh@gmail.com>
2025-01-09 05:11:31 -0800

    up

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,4 @@ urlpatterns = [
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('municipal_dashboard/', views.municipal_dashboard, name='municipal_dashboard'),
     path('add-bin/', views.add_bin, name='add_bin'),
-
-    # path('edit_bin/<str:bin_id>/', views.edit_bin, name='edit_bin'),
-    # path('delete_bin/<str:bin_id>/', views.delete_bin, name='delete_bin'),
 ]
